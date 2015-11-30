06da080172a47efa75f99d9b9ac2cd58ffba353d
halloworld <halloworld@192.168.0.102>
2015-11-30 22:39:41 +0800

    登录功能Stream改造

--- MyXMPP/MyXMPP/LoginViewController.m
+++ MyXMPP/MyXMPP/LoginViewController.m
@@ -7,7 +7,47 @@
 //
 
 #import "LoginViewController.h"
+#import "XMPPManager.h"
+
+@interface LoginViewController () <XMPPStreamDelegate>
+
+@property (nonatomic, assign) IBOutlet UITextField *userField;
+@property (nonatomic, assign) IBOutlet UITextField *passwordField;
+
+@end
 
 @implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (IBAction)btnLoginTap:(id)sender {
+    [[XMPPManager shareInterface] addStreamDelegate:self];
+    [[XMPPManager shareInterface] connect];
+}
+
+
+#pragma mark - XMPPStream Delegate
+
+- (void)xmppStreamDidConnect:(XMPPStream *)sender {
+    NSString *username = self.userField.text;
+    NSString *password = self.passwordField.text;
+    [sender setMyJID:[XMPPJID jidWithUser:username domain:kHostName resource:nil]];
+    NSError *err = nil;
+    BOOL res = [sender authenticateWithPassword:password error:&err];
+    if (!res) {
+        NSLog(@"%s -> %@", __FUNCTION__, [err description]);
+    }
+}
+
+
+- (void)xmppStreamDidAuthenticate:(XMPPStream *)sender {
+    NSLog(@"%s", __FUNCTION__);
+}
+
+- (void)xmppStream:(XMPPStream *)sender didNotAuthenticate:(DDXMLElement *)error {
+    NSLog(@"%s -> %@", __FUNCTION__, [error prettyXMLString]);
+}
 
 @end
