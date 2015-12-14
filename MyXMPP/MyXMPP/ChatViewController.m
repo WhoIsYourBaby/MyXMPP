d6c1a69e1fb467021fbfbe3e238e06514274cbb7
halloworld <halloworld@192.168.0.102>
2015-12-14 22:30:45 +0800

    单对单文本聊天ok

--- MyXMPP/MyXMPP/ChatViewController.m
+++ MyXMPP/MyXMPP/ChatViewController.m
@@ -8,7 +8,7 @@
 
 #import "ChatViewController.h"
 
-@interface ChatViewController ()
+@interface ChatViewController () <XMPPStreamDelegate>
 
 @property (nonatomic, strong) XMPPJID *friendJid;
 @property (nonatomic, weak) IBOutlet UITextField *sendText;
@@ -20,6 +20,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [[XMPPManager shareInterface] addStreamDelegate:self];
 }
 
 - (void)setChatFriendJid:(XMPPJID *)aJid {
@@ -27,6 +28,36 @@
 }
 
 - (IBAction)btnSendTap:(id)sender {
+    NSString *chatContent = self.sendText.text;
+    NSString *user = [[[[XMPPManager shareInterface] xmppStream] myJID] user];
+    [self showChatText:chatContent from:user];
+    [[UIApplication sharedApplication] sendAction:@selector(resignFirstResponder) to:nil from:nil forEvent:nil];
+    
+    [self sendContent:chatContent];
+    
+    self.sendText.text = @"";
+}
+
+- (void)sendContent:(NSString *)text {
+    NSXMLElement *body = [NSXMLElement elementWithName:@"body" stringValue:text];
+    XMPPMessage *msg = [XMPPMessage messageWithType:@"chat" to:self.friendJid];
+    [msg addChild:body];
+    [[[XMPPManager shareInterface] xmppStream] sendElement:msg];
+}
+
+
+- (void)showChatText:(NSString *)chatString from:(NSString *)aUser {
+    NSString *chatContent = self.chatText.text;
+    chatContent = [chatContent stringByAppendingFormat:@"%@ : %@\n", [self.friendJid user], self.sendText.text];
+    self.chatText.text = chatContent;
+}
+
+#pragma mark - 收到消息
+
+- (void)xmppStream:(XMPPStream *)sender didReceiveMessage:(XMPPMessage *)message {
+    NSString *user = [[message from] user];
+    NSString *chatContent = [message body];
+    [self showChatText:chatContent from:user];
 }
 
 @end
