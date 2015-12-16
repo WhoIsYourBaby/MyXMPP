3cfda5479df75748893258e025978b20500672f2
halloworld <halloworld@192.168.0.102>
2015-12-16 22:18:55 +0800

    开始重构sb故事版添加TabbarVC

---
+++ MyXMPP/MyXMPP/RoomChatViewController.h
@@ -0,0 +1,13 @@
+//
+//  RoomChatViewController.h
+//  MyXMPP
+//
+//  Created by halloworld on 15/12/16.
+//  Copyright © 2015年 halloworld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RoomChatViewController : UIViewController
+
+@end
---
+++ MyXMPP/MyXMPP/RoomChatViewController.m
@@ -0,0 +1,17 @@
+//
+//  RoomChatViewController.m
+//  MyXMPP
+//
+//  Created by halloworld on 15/12/16.
+//  Copyright © 2015年 halloworld. All rights reserved.
+//
+
+#import "RoomChatViewController.h"
+
+@implementation RoomChatViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
