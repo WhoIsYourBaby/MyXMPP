04af102f5016fd04ab451281240c2ed01a241552
halloworld <halloworld@192.168.0.102>
2016-01-10 17:50:20 +0800

    vcard UI

--- MyXMPP/MyXMPP/VCardViewController.m
+++ MyXMPP/MyXMPP/VCardViewController.m
@@ -8,7 +8,7 @@
 
 #import "VCardViewController.h"
 
-@interface VCardViewController ()
+@interface VCardViewController () <UIImagePickerControllerDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *nicknameText;
 @property (weak, nonatomic) IBOutlet UITextField *emailText;
@@ -17,5 +17,23 @@
 @end
 
 @implementation VCardViewController
+
+
+- (IBAction)btnHeaderImageTap:(id)sender {
+    UIImagePickerController *imgPicker = [[UIImagePickerController alloc] init];
+    imgPicker.delegate = self;
+    imgPicker.allowsEditing = YES;
+    [self.navigationController presentViewController:imgPicker animated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    UIImage *img = info[UIImagePickerControllerEditedImage];
+    [self.headerButton setImage:img forState:UIControlStateNormal];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
 
 @end
