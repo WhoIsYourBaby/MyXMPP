236f95ff4207613c6d5a6e84b76c9323431b4c52
halloworld <halloworld@192.168.0.102>
2016-01-10 17:35:51 +0800

    Vacard

---
+++ MyXMPP/MyXMPP/VCardViewController.h
@@ -0,0 +1,13 @@
+//
+//  VCardViewController.h
+//  MyXMPP
+//
+//  Created by halloworld on 16/1/10.
+//  Copyright © 2016年 halloworld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCardViewController : UIViewController
+
+@end
---
+++ MyXMPP/MyXMPP/VCardViewController.m
@@ -0,0 +1,21 @@
+//
+//  VCardViewController.m
+//  MyXMPP
+//
+//  Created by halloworld on 16/1/10.
+//  Copyright © 2016年 halloworld. All rights reserved.
+//
+
+#import "VCardViewController.h"
+
+@interface VCardViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *nicknameText;
+@property (weak, nonatomic) IBOutlet UITextField *emailText;
+@property (weak, nonatomic) IBOutlet UIButton *headerButton;
+
+@end
+
+@implementation VCardViewController
+
+@end
